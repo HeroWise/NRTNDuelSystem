5346d81948fa2ca38f7465a2a9e4b3cec17f99d7
Hero <heroversewizard@gmail.com>
2017-10-02 14:16:58 +1100

    Added Arena Manager and Arena Class

--- src/com/topobon/duel/arena/Arena.java
+++ src/com/topobon/duel/arena/Arena.java
@@ -1,75 +1,43 @@
+
 package com.topobon.duel.arena;
+import org.bukkit.Location;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-import org.bukkit.entity.Player;
-
+// Also NOT thread safe
 public class Arena {
-	private int arenaID;
-	private List<Player> players;
-	private List<Player> invitations;
+    // Individual arena info here
+
+    // Ofc, this CAN'T be the ID COULD IT? (jk)
+    private final int id;
+    private final Location locA;
+    private final Location locB;
+    private final List<UUID> players = new ArrayList<UUID>();
+
+    public Arena(Location location1, Location location2, int id) {
+        this.locA = location1;
+        this.locB = location2;
+        this.id = id;
+    }
+
+    // Getters
+
+    public int getId() {
+        return this.id;
+    }
+
+    public List<UUID> getPlayers() {
+        return this.players;
+    }
+
+	public Location getLocationA() {
+		return locA;
+	}
 	
-	public Arena(int arenaID) {
-		this.arenaID = arenaID;
-		this.players = new ArrayList<Player>();
-		this.invitations = new ArrayList<Player>();
+
+	public Location getLocationB() {
+		return locB;
 	}
-//
-//	public boolean isLeader(Player player) {
-//		return this.leader == player;
-//	}
-
-	public List<Player> getPlayers() {
-		return this.players;
-	}
-
-//	public Player getLeader() {
-//		return this.leader;
-//	}
-
-	public boolean doesArenaExist(int idNumberForArena) {
-			
-		if (this.arenaID == idNumberForArena) {
-			return true;
-		}
-		
-		return false;
-	}
-
-	public boolean addPlayer(Player player) {
-		if ((!this.players.contains(player)) && (this.invitations.contains(player))) {
-			this.players.add(player);
-			this.invitations.remove(player);
-			return true;
-		}
-		return false;
-	}
-
-	public boolean removePlayer(Player player) {
-		if (this.players.contains(player)) {
-			this.players.remove(player);
-			return true;
-		}
-		return false;
-	}
-
-//	public void invite(final Player p) {
-//		this.invitations.add(p);
-//		p.sendMessage("You are invited in" + this.leader.getName() + "'s party.");
-//		p.sendMessage("" + this.leader.getName());
-//		new BukkitRunnable() {
-//
-//			public void run() {
-//				if (Party.this.invitations.contains(p)) {
-//					Party.this.invitations.remove(p);
-//					p.sendMessage("7Your invitations has been expired.");
-//					Party.this.leader.sendMessage("The invitation to " + p.getName() + " has been expired.");
-//				}
-//			}
-//		};
-//
-//	}
 }
-
-
--- src/com/topobon/duel/arena/ArenaManager.java
+++ src/com/topobon/duel/arena/ArenaManager.java
@@ -1,66 +1,167 @@
 package com.topobon.duel.arena;
 
-import java.awt.List;
-import java.util.ArrayList;
-
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-public class ArenaManager {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-	  private static ArrayList<Arena> arenas = new ArrayList<Arena>();
-	  
-	  public static Arena getArena(Integer arenaID)
-	  {
-	    for (Arena arena : arenas) {
-	      if (arena.doesArenaExist(arenaID)) {
-	        return arena;
-	      }
-	    }
-	    return null;
-	  }
-	  
-	  public static boolean createArena(Integer arenaID)
-	  {
-	    if (getArena(arenaID) == null)
-	    {
-	      arenas.add(new Arena(arenaID));
-	      return true;
-	    }
-	    return false;
-	  }
-	  
-	  public static boolean deleteParty(Player player)
-	  {
-	    if (getParty(player) != null)
-	    {
-	      if (getParty(player).isLeader(player))
-	      {
-	        for (Player p : getParty(player).getPlayers()) {
-	          getParty(player).removePlayer(p);
-	        }
-	        parties.remove(getParty(player));
-	        return true;
-	      }
-	      return false;
-	    }
-	    return false;
-	  }
-	  
-	  public static List<Party> getParties()
-	  {
-	    return parties;
-	  }
-	  
-	  public static void deleteParty(Party party)
-	  {
-	    if (party != null)
-	    {
-	      for (int i = 0; i < party.getPlayers().size(); i++) {
-	        if (party.getPlayers().get(i) != null) {
-	          party.getPlayers().remove(i);
-	        }
-	      }
-	      parties.remove(party);
-	    }
-	  }
-	}
+// NOT THREAD SAFE!
+public class ArenaManager{
+    // Singleton instance
+    private static ArenaManager am;
+
+    // Player data
+    private final Map<UUID, Location> locs = new HashMap<UUID, Location>();
+  //  private final Map<UUID, ItemStack[]> inv = new HashMap<UUID, ItemStack[]>();
+  //  private final Map<UUID, ItemStack[]> armor = new HashMap<UUID, ItemStack[]>();
+
+   
+    private final List<Arena> arenas = new ArrayList<Arena>();
+    // Keep track of the current arena ID
+    private int arenaSize = 0;
+
+    private ArenaManager() {} // Prevent instantiation
+
+    // Singleton accessor with lazy initialization
+    public static ArenaManager getManager() {
+        if (am == null)
+            am = new ArenaManager();
+
+        return am;
+    }
+
+    /**
+     * Acquires an arena based on its ID number
+     *
+     * @param i the ID to search the arenas for
+     * @return the arena possessing the specified ID
+     */
+    public Arena getArena(int i){
+        for (Arena a : this.arenas) {
+            if (a.getId() == i) {
+                return a;
+            }
+        }
+
+        return null; // Not found
+    }
+
+    /**
+     * Adds the player to an arena
+     *
+     * <p>Gets the arena by ID, checks that it exists,
+     * and check the player isn't already in a game.</p>
+     *
+     * @param p the player to add
+     * @param i the arena ID. A check will be done to ensure its validity.
+     */
+    public void addPlayer(Player p, int i) {
+        Arena a = this.getArena(i);
+        if (a == null) {
+            p.sendMessage("Invalid arena!");
+            return;
+        }
+
+        if (this.isInGame(p)) {
+            p.sendMessage("Cannot join more than 1 game!");
+            return;
+        }
+
+        // Adds the player to the arena player list
+        a.getPlayers().add(p.getUniqueId());
+
+        
+        // Clear inventory and armor
+  //      p.getInventory().setArmorContents(null);
+   //     p.getInventory().clear();
+
+        // Save the players's last location before joining,
+        // then teleporting them to the arena spawn
+        locs.put(p.getUniqueId(), p.getLocation());
+       
+        if(a.getPlayers().size()==0){
+        	  p.teleport(a.getLocationA());
+        } else {
+        	  p.teleport(a.getLocationB());
+        }
+      
+    }
+
+    /**
+     * Removes the player from their current arena.
+     *
+     * <p>The player is allowed to not be in game, a check
+     * will be performed to ensure the validity of the arena</p>
+     *
+     * @param p the player to remove from the arena
+     */
+    public void removePlayer(Player p) {
+        Arena a = null;
+
+        // Searches each arena for the player
+        for (Arena arena : this.arenas) {
+            if (arena.getPlayers().contains(p.getUniqueId()))
+                a = arena;
+        }
+
+        // Check arena validity
+        if (a == null) {
+            p.sendMessage("Invalid operation!");
+            return;
+        }
+
+        // Remove from arena player lost
+        a.getPlayers().remove(p.getName());
+
+        // Teleport to original location, remove it too
+        p.teleport(locs.get(p.getUniqueId()));
+        locs.remove(p.getUniqueId());
+  
+        // Heh, you're safe now :)
+        p.setFireTicks(0);
+    }
+
+    /**
+     * Creates an arena at the specified location
+     *
+     * @param l the location for arena spawn
+     * @return the arena created
+     */
+    public Arena createArena(Location l1, Location l2) {       
+        this.arenaSize++;
+
+        Arena a = new Arena(l1,l2, this.arenaSize);
+        this.arenas.add(a);
+
+        return a;
+    }
+
+    /**
+     * Checks if the player is currently in an arena
+     *
+     * @param p the player to check
+     * @return {@code true} if the player is in game
+     */
+    public boolean isInGame(Player p) {
+        for (Arena a : this.arenas) {
+            if (a.getPlayers().contains(p.getUniqueId()))
+                return true;
+        }
+        return false;
+    }
+
+    public String serializeLoc(Location l){
+        return l.getWorld().getName() + "," + l.getBlockX() + "," + l.getBlockY() + "," + l.getBlockZ();
+    }
+
+    public Location deserializeLoc(String s){
+        String[] st = s.split(",");
+        return new Location(Bukkit.getWorld(st[0]), Integer.parseInt(st[1]), Integer.parseInt(st[2]), Integer.parseInt(st[3]));
+    }
+}
